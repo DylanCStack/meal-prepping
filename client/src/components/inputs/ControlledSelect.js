7c75c6608449bba6a8ac04177b95468b86f9dad6
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-04-13 21:56:09 -0700

    add ControlledSelect component

---
+++ client/src/components/inputs/ControlledSelect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ControlledSelect(props) {
+  return (
+    <select value={props.selected} onChange={ e => props.handleChange(props.property, e)}>
+      {props.options.map(option => { // return jsx for options array
+        return <option value={option.value}>{option.displayName}</option>
+      })}
+    </select>
+  )
+}
