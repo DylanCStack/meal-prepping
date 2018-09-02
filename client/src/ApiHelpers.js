cc94d7912f5c79f94a68f9a520cb5553cc1c2969
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-09-02 13:26:58 -0700

    move api calls to their own helper class

---
+++ client/src/ApiHelpers.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export default class Api {
+  createIngredient = (ingredient) => {
+    return this.createEntity('/api/ingredients/add', 'ingredients', ingredient);
+  }
+
+  deleteIngredient = (e, id) => {
+    return this.deleteEntity('/api/ingredients/delete', 'ingredients', id);
+  }
+  
+  createRecipe = (recipe) => {
+    return this.createEntity('/api/recipes/add', 'recipes', recipe);
+  }
+  
+  deleteRecipe = (e, id) => {
+    return this.deleteEntity('/api/recipes/delete', 'recipes', id);
+  }
+  
+  createEntity = (endpoint, property, entity, success = ()=>{}, error = ()=>{}) => {
+    return axios.post(endpoint, entity)
+      .then(res => {
+        return success(property);
+      }).catch(err => {
+        console.log(err);
+        return error(property, err);
+    });
+  }
+  deleteEntity = (endpoint, property, id, success = ()=>{}, error = ()=>{}) => {
+    return axios.post(endpoint, {
+        id
+      }).then(res => {
+        return success(property, res);
+      }).catch(err => {
+        return error(property, err);
+      });
+  }
+}
