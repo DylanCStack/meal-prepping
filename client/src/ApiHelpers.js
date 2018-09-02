7874f054938dfa0dd2b25a21afe3cc6ec1a53bc1
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-09-02 14:43:16 -0700

    change arrow functions to bound methods

--- client/src/ApiHelpers.js
+++ client/src/ApiHelpers.js
@@ -1,23 +1,29 @@
 import axios from 'axios';
 
 export default class Api {
-  createIngredient = (ingredient) => {
+  constructor() {
+    this.createIngredient = this.createIngredient.bind(this);
+    this.deleteIngredient = this.deleteIngredient.bind(this);
+    this.createRecipe = this.createRecipe.bind(this);
+    this.deleteRecipe = this.deleteRecipe.bind(this);
+  }
+  createIngredient (ingredient) {
     return this.createEntity('/api/ingredients/add', 'ingredients', ingredient);
   }
 
-  deleteIngredient = (e, id) => {
+  deleteIngredient (e, id) {
     return this.deleteEntity('/api/ingredients/delete', 'ingredients', id);
   }
   
-  createRecipe = (recipe) => {
+  createRecipe (recipe) {
     return this.createEntity('/api/recipes/add', 'recipes', recipe);
   }
   
-  deleteRecipe = (e, id) => {
+  deleteRecipe (e, id) {
     return this.deleteEntity('/api/recipes/delete', 'recipes', id);
   }
   
-  createEntity = (endpoint, property, entity, success = ()=>{}, error = ()=>{}) => {
+  createEntity (endpoint, property, entity, success = ()=>{}, error = ()=>{}) {
     return axios.post(endpoint, entity)
       .then(res => {
         return success(property);
@@ -26,7 +32,7 @@ export default class Api {
         return error(property, err);
     });
   }
-  deleteEntity = (endpoint, property, id, success = ()=>{}, error = ()=>{}) => {
+  deleteEntity (endpoint, property, id, success = ()=>{}, error = ()=>{}) {
     return axios.post(endpoint, {
         id
       }).then(res => {
