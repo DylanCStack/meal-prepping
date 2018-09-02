7147fa02578095bafee34194554005366892e550
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-09-02 13:28:06 -0700

    add empty RecipeDetail component

---
+++ client/src/components/RecipeDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class RecipeDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className='recipe'>
+        stuff
+      </div>
+    );
+  }
+}
